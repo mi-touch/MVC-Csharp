178552ab6f5c94cce3132b3a53578c64d67b3a0f
Timileyin Adefare <61581959+mi-touch@users.noreply.github.com>
2024-11-23 18:18:09 +0100

    Update HelloWorldController.cs

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -1,27 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Encodings.Web;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
+        // Default action: renders the Index view
         public IActionResult Index()
         {
             return View();
         }
 
-        // 
-        // GET: /HelloWorld/Welcome/ 
-
-        // GET: /HelloWorld/Welcome/ 
-        // Requires using System.Text.Encodings.Web;
+        // GET: /HelloWorld/Welcome/
         public IActionResult Welcome(string name, int numTimes = 1)
         {
-            ViewData["Message"] = "Hello " + name;
+            ViewData["Message"] = $"Hello {name}";
             ViewData["NumTimes"] = numTimes;
 
             return View();
